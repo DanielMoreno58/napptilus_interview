9214b35002de4eea70f0590740ab10c20d34d4f4
Daniel Moreno <daniel10697@hotmail.com>
2022-09-05 13:06:51 +0200

    docs: add swagger to api

--- src/main/java/com/example/napptilus/NapptilusApplication.java
+++ src/main/java/com/example/napptilus/NapptilusApplication.java
@@ -2,8 +2,10 @@ package com.example.napptilus;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
+@EnableWebMvc
 public class NapptilusApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/example/napptilus/rest/PriceController.java
+++ src/main/java/com/example/napptilus/rest/PriceController.java
@@ -2,6 +2,7 @@ package com.example.napptilus.rest;
 
 import com.example.napptilus.model.entity.Price;
 import com.example.napptilus.model.service.IPriceService;
+import io.swagger.annotations.Api;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@Api(tags = "knows how receive manage user requests")
 @RestController
 @RequestMapping(value="price")
 public class PriceController {
